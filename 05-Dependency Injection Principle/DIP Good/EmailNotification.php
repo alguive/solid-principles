4da802b606c6af1aed2182b45a5579876dbff45a
alguive <a.guijarro.vera@gmail.com>
2024-10-19 13:00:26 +0200

    Fix typo

--- 05-Dependency Injection Principle/DIP Good/EmailNotification.php
+++ 05-Dependency Injection Principle/DIP Good/EmailNotification.php
@@ -4,6 +4,6 @@ class EmailNotification implements NotificationInterface
 {
     public function send(string $message): void
     {
-        echo \sprinf('Sending email notification: %s', $message);
+        echo \sprintf('Sending email notification: %s', $message);
     }
 }
